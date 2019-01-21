2cedf22264acd3ec218e9e90bab0615804e6fc22
Tran <lt2415@ic.ac.uk>
2019-01-21 11:33:45 +0000

    Updated content

---
+++ scripts/DOM.js
@@ -0,0 +1,27 @@
+/* Document Ready */
+$( document ).ready(function() {
+
+  /* Set default navbar tab*/
+  location.hash = '#introduction';
+  $(".nav li").removeClass('active');
+  $("#introduction-tab").addClass('active');
+
+});
+
+/* Dynamic onhashchange event */
+window.addEventListener("hashchange", function() {
+
+  /* Set active navbar tab */
+  var tabID = window.location.hash.concat('-tab');
+
+  $(".nav li").removeClass('active');
+  $(tabID).addClass('active');
+
+  /* Display/hide content based on hash */
+  var hashID = window.location.hash.concat('-content');
+
+  $("#navbar-content").children().fadeOut(250).promise().done( function() {
+    $(hashID).fadeIn(250);
+  });
+
+});
