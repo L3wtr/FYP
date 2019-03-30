22a03d76a1ac87380f07276a5a2809ad4e107c04
Unknown <lewistran96@gmail.com>
2019-03-30 13:24:48 +0000

    Functionally complete (minus zoom)

--- js/DOM.js
+++ js/DOM.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
   $("#introduction-tab").addClass('active');
 
   // Beta modal
-  $('#betaModal').modal('show');
+  // $('#betaModal').modal('show');
 
   // Disable run simulation on Edge browsers
   if (navigator.userAgent.indexOf('Edge') >= 0){
@@ -131,6 +131,48 @@ $("#datum-pa, #datum-pb").on('click', function() {
   }
 });
 
+/* Poorly designed housing fastener radio button group event ---------------- */
+$("#datum-ha, #datum-hb").on('click', function() {
+
+  // Clear previously selected
+  $("#datum-ha, #datum-hb").removeClass('btn-info');
+  $("#datum-ha, #datum-hb").addClass('btn-outline-info');
+
+  // Toggle button class for clarification
+  $(this).toggleClass('btn-outline-info btn-info');
+
+  // Change drawing source
+  var datumVal = $('input[name=Datum-H]:checked').val();
+
+  $('#poor-housing-figure').find("img").each(function() {
+    // File path
+    var filePath = this.src;
+
+    // Path index up to x.png
+    var fileIndex = filePath.length - 5;
+
+    this.src = filePath.substr(0, fileIndex) + datumVal + filePath.substr(fileIndex + 1);
+  });
+
+  if (datumVal === "b") {
+    $("#housing-datum-text").text("Hex Bolt Fasteners");
+  }
+  else {
+    $("#housing-datum-text").text("Pin Fasteners");
+  }
+
+  // Update assembly drawing
+  $('#simulation-figure').find("img").each(function() {
+    // File path
+    var filePath = this.src;
+
+    // Path index up to x.png
+    var fileIndex = filePath.length - 5;
+
+    this.src = filePath.substr(0, fileIndex) + datumVal + filePath.substr(fileIndex + 1);
+  });
+});
+
 /* Misalignment drawing radio button group event ---------------------------- */
 $("#all-drawing, #axial-drawing, #radial-drawing").on('click', function() {
 
@@ -161,6 +203,7 @@ $("#all-drawing, #axial-drawing, #radial-drawing").on('click', function() {
       // Show all well and poor datums if all display
       $("#datum-wa, #datum-wb").show();
       $("#datum-pa, #datum-pb").show();
+      $("#datum-ha, #datum-hb").show();
 
       // Reset poor datum position
       if (datumValP === "a") {
@@ -173,6 +216,7 @@ $("#all-drawing, #axial-drawing, #radial-drawing").on('click', function() {
       // Show poor and hide well datums if axial display
       $("#datum-wa, #datum-wb").hide();
       $("#datum-pa, #datum-pb").show();
+      $("#datum-ha, #datum-hb").hide();
 
       // Change poor datum position
       if (datumValP === "a") {
@@ -187,6 +231,7 @@ $("#all-drawing, #axial-drawing, #radial-drawing").on('click', function() {
       // Hide all well and poor datums if radial display
       $("#datum-wa, #datum-wb").hide();
       $("#datum-pa, #datum-pb").hide();
+      $("#datum-ha, #datum-hb").show();
       break
   }
 });
@@ -213,18 +258,44 @@ $("#100units, #1000units, #10000units").on('click', function() {
   $(this).toggleClass('btn-outline-info btn-info');
 });
 
+/* Custom simulation radio button group event ------------------------------- */
+$("#100units-custom, #1000units-custom, #10000units-custom").on('click', function() {
+
+  // Clear previously selected
+  $("#100units-custom, #1000units-custom, #10000units-custom").removeClass('btn-info');
+  $("#100units-custom, #1000units-custom, #10000units-custom").addClass('btn-outline-info');
+
+  // Toggle button class for clarification
+  $(this).toggleClass('btn-outline-info btn-info');
+});
+
 /* Simulation run onclick event --------------------------------------------- */
 $("#sim-button").on('click', function() {
 
   var units = $('input[name=Units]:checked').val();
 
   // Simulate stackup model
-  stackup(units);
+  stackup(units, false);
   $("#simulation-plot").hide();
 
   $("#simulation-settings").fadeOut(250).promise().done( function() {
       $("#simulation-plot").fadeTo(250,100);
       $("#simulation-again").fadeIn(250);
+  });
+});
+
+/* Custom simulation run onclick event -------------------------------------- */
+$("#sim-button-custom").on('click', function() {
+
+  var units = $('input[name=Units-custom]:checked').val();
+
+  // Simulate stackup model
+  stackup(units, true);
+  $("#simulation-plot-custom").hide();
+
+  $("#simulation-settings-custom").fadeOut(250).promise().done( function() {
+      $("#simulation-plot-custom").fadeTo(250,100);
+      $("#simulation-again-custom").fadeIn(250);
   });
 });
 
@@ -234,7 +305,16 @@ $("#sim-button-again").on('click', function() {
   var units = $('input[name=Units]:checked').val();
 
   // Simulate stackup model
-  stackup(units);
+  stackup(units, false);
+});
+
+/* Custom simulation run again onclick event -------------------------------- */
+$("#sim-button-again-custom").on('click', function() {
+
+  var units = $('input[name=Units-custom]:checked').val();
+
+  // Simulate stackup model
+  stackup(units, true);
 });
 
 /* Simulation reset onclick event ------------------------------------------- */
@@ -247,4 +327,30 @@ $("#sim-button-reset").on('click', function() {
   $("#simulation-again").fadeOut(250).promise().done( function() {
     $("#simulation-settings").fadeIn(250);
   });
+});
+
+/* Custom simulation reset onclick event ------------------------------------ */
+$("#sim-button-reset-custom").on('click', function() {
+
+  $("#simulation-plot-custom").fadeTo(250,0).promise().done( function() {
+    $("#simulation-plot-custom").empty();
+  });
+
+  $("#simulation-again-custom").fadeOut(250).promise().done( function() {
+    $("#simulation-settings-custom").fadeIn(250);
+  });
+
+  // Clear table data
+  $("#data-table-custom td").empty();
+
+});
+
+/* Custom simulation reset values onclick event ----------------------------- */
+$("#bounds-reset").on('click', function() {
+
+  $('#bounds-container input').each(function() {
+    // Set bounds values to zero
+    $(this).val(0);
+  });
+
 });
--- js/simulation.js
+++ js/simulation.js
@@ -18,6 +18,7 @@ function randNorm(lower, upper, n) {
   // Approxiate mate a normal distirbution from a uniform one up to the degree, d
   // [(d * U(0,1) - d/2) / (d/2)] * limit
   popMat = math.random([n, degree]);
+
   popNorm = math.add(math.multiply(math.divide(math.subtract(math.sum(popMat, 1), degree/2), degree/2), limit), shift);
 
   popArr = popNorm.flat(); // Flattens matrix
@@ -26,73 +27,9 @@ function randNorm(lower, upper, n) {
 }
 
 /* Stackup simulation method */
-function stackup(units) {
+function stackup(units, custom) {
   let tolWell, tolPoor;
   let tolWellMean, tolWellSTD, tolPoorMean, tolPoorSTD;
-
-  var direction = $('input[name=Direction]:checked').val();
-  var datumVal = $('input[name=Datum-P]:checked').val();
-
-  switch (direction) {
-    case 'axial':
-      // Well designed tolerance contribution (unaffected by datum)
-      tolWell = math.add(randNorm(-0.1, 0.1, units), randNorm(-0.1, 0.1, units), randNorm(-0.1, 0.1, units));
-
-      // Poorly designed tolerance contribution (directed by datum)
-      tolPoor = math.add(randNorm(-0.1, 0.1, units), randNorm(-0.1, 0.1, units), randNorm(-0.1, 0.1, units),  // Locating grooves
-                          randNorm(-0.06, 0, units), randNorm(-0.06, 0, units), randNorm(-0.06, 0, units),    // Internal circlips
-                            randNorm(0, 0.1, units), randNorm(0, 0.1, units), randNorm(0, 0.1, units),        // Internal groove size
-                              randNorm(-0.06, 0, units), randNorm(-0.06, 0, units),         // External circlips
-                                randNorm(0, 0.1, units), randNorm(0, 0.1, units),           // External groove size
-                                  randNorm(-0.1, 0.1, units), randNorm(-0.1, 0.1, units));  // Housing lip
-
-      // Add additional tolerances for datum A
-      if (datumVal === 'a' ) {
-        tolPoor = math.add(tolPoor, randNorm(-0.1, 0.1, units), randNorm(-0.06, 0, units), randNorm(0, 0.1, units)); // Additional circlip and grooves
-      }
-
-      // Update table with statistics
-      tableData('data-row1', tolWell, 0.1);
-      tableData('data-row2', tolPoor, 0.1);
-    break
-
-    case 'radial':
-      // Well designed tolerance contribution
-      tolWell = math.add(randNorm(-0.004, 0.009, units),  // Shaft contribution
-                          randNorm(-0.1, 0.1, units), randNorm(-0.006, 0.010, units));  // Housing contribution
-
-      // Poorly designed tolerance contribution
-      tolPoor = math.add(randNorm(-0.004, 0.009, units),  // Shaft contribution
-                          randNorm(-0.1, 0.1, units), randNorm(-0.006, 0.010, units), // Housing contribution
-                            randNorm(-0.029, 0.004, units), randNorm(-0.029, 0.004, units));  // Bore contribution
-
-      // Update table with statistics
-      tableData('data-row1', tolWell, 0.05);
-      tableData('data-row2', tolPoor, 0.05);
-    break
-  }
-
-  /* Histogram plot */
-  // Simulated data
-  let traceWell = {
-        x: tolWell,
-        type: 'histogram',
-        name: 'Well Designed',
-        marker: {
-          color: 'rgba(41, 72, 102, 0.8)'
-        },
-      };
-
-  let tracePoor = {
-        x: tolPoor,
-        type: 'histogram',
-        name: 'Poorly Designed',
-        marker: {
-          color: 'rgba(247, 151, 95, 0.8)'
-        },
-      };
-
-  let data = [traceWell, tracePoor];
 
   let layout = {
     //autosize: true,
@@ -130,7 +67,143 @@ function stackup(units) {
     paper_bgcolor: 'rgba(0,0,0,0)'
   };
 
-  Plotly.newPlot('simulation-plot', data, layout,  {displayModeBar: false});
+  if (custom) {
+    var bounds = {};
+    let i = 0;
+    let traceSet1 = {type: 'histogram'};
+    let traceSet2 = {type: 'histogram'};
+
+    $("form input").each(function() {
+      bounds[i] = parseFloat($(this).val());
+      i++;
+    });
+
+    // Mapping bounds to standard sets
+    set1 = [bounds[1], bounds[0], bounds[3], bounds[2], bounds[5], bounds[4]];
+    set2 = [bounds[7], bounds[6], bounds[9], bounds[8], bounds[11], bounds[10]];
+
+    // Rejection criteria
+    let rej = parseFloat($("#reject-custom").val());
+
+    // Zero check function
+    function zeroCheck(val) {
+      return val == 0;
+    }
+
+    if (!set1.every(zeroCheck)) {
+      // Tolerance set 1
+      let tolSet1 = math.add(randNorm(set1[0], set1[1], units),
+                              randNorm(set1[2], set1[3], units),
+                                randNorm(set1[4], set1[5], units));
+
+      traceSet1 = {
+        x: tolSet1,
+        type: 'histogram',
+        name: 'Set 1',
+        marker: {
+          color: 'rgba(41, 72, 102, 0.8)'
+        },
+      };
+
+      // Update table with statistics
+      tableData('data-row1-custom', tolSet1, rej);
+    }
+
+    if (!set2.every(zeroCheck)) {
+      // Tolerance set 2
+      let tolSet2 = math.add(randNorm(set2[0], set2[1], units),
+                              randNorm(set2[2], set2[3], units),
+                                randNorm(set2[4], set2[5], units));
+
+      traceSet2 = {
+        x: tolSet2,
+        type: 'histogram',
+        name: 'Set 2',
+        marker: {
+          color: 'rgba(247, 151, 95, 0.8)'
+        },
+      };
+
+      // Update table with statistics
+      tableData('data-row2-custom', tolSet2, rej);
+    }
+
+    let data = [traceSet1, traceSet2];
+
+    Plotly.newPlot('simulation-plot-custom', data, layout, {displayModeBar: false});
+  }
+  else {
+    var direction = $('input[name=Direction]:checked').val();
+    var datumVal = $('input[name=Datum-P]:checked').val();
+    var fastVal = $('input[name=Datum-H]:checked').val();
+
+    switch (direction) {
+      case 'axial':
+        // Well designed tolerance contribution (unaffected by datum)
+        tolWell = math.add(randNorm(-0.1, 0.1, units), randNorm(-0.1, 0.1, units), randNorm(-0.1, 0.1, units));
+
+        // Poorly designed tolerance contribution (directed by datum)
+        tolPoor = math.add(randNorm(-0.1, 0.1, units), randNorm(-0.1, 0.1, units), randNorm(-0.1, 0.1, units),  // Locating grooves
+                            randNorm(-0.06, 0, units), randNorm(-0.06, 0, units), randNorm(-0.06, 0, units),    // Internal circlips
+                              randNorm(0, 0.14, units), randNorm(0, 0.14, units), randNorm(0, 0.14, units),     // Internal groove size
+                                randNorm(-0.06, 0, units), randNorm(-0.06, 0, units),         // External circlips
+                                  randNorm(0, 0.14, units), randNorm(0, 0.14, units),         // External groove size
+                                    randNorm(-0.1, 0.1, units), randNorm(-0.1, 0.1, units));  // Housing lip
+
+        // Add additional tolerances for datum A
+        if (datumVal === 'a' ) {
+          tolPoor = math.add(tolPoor, randNorm(-0.1, 0.1, units), randNorm(-0.06, 0, units), randNorm(0, 0.14, units)); // Additional circlip and grooves
+        }
+
+        // Update table with statistics
+        tableData('data-row1', tolWell, 0.1);
+        tableData('data-row2', tolPoor, 0.1);
+      break
+
+      case 'radial':
+        // Well designed tolerance contribution
+        tolWell = math.add(randNorm(-0.004, 0.009, units),  // Shaft contribution
+                            randNorm(-0.1, 0.1, units), randNorm(0, 0.025, units));  // Housing contribution
+
+        // Poorly designed tolerance contribution
+        tolPoor = math.add(randNorm(-0.004, 0.009, units),  // Shaft contribution
+                            randNorm(-0.1, 0.1, units), randNorm(-0.1, 0.1, units), randNorm(0, 0.025, units)); // Housing contribution (hole location and bearing bore)
+
+        // Add additional tolerances for bolt fasteners
+        if (fastVal === 'b' ) {
+          tolPoor = math.add(tolPoor, randNorm(-0.1, 0.1, units), randNorm(-0.1, 0.1, units)); // Bore contribution
+        }
+
+        // Update table with statistics
+        tableData('data-row1', tolWell, 0.05);
+        tableData('data-row2', tolPoor, 0.05);
+      break
+    }
+
+    /* Histogram plot */
+    // Simulated data
+    let traceWell = {
+          x: tolWell,
+          type: 'histogram',
+          name: 'Well Designed',
+          marker: {
+            color: 'rgba(41, 72, 102, 0.8)'
+          },
+        };
+
+    let tracePoor = {
+          x: tolPoor,
+          type: 'histogram',
+          name: 'Poorly Designed',
+          marker: {
+            color: 'rgba(247, 151, 95, 0.8)'
+          },
+        };
+
+    let data = [traceWell, tracePoor];
+
+    Plotly.newPlot('simulation-plot', data, layout,  {displayModeBar: false});
+  }
 }
 
 /* Calculate data and update table */
